ac9879930fff81e0d170e01638135426c7d0355b
Nikolai Polevoi <nikolai_polevoi@epam.com>
2021-11-11 17:41:58 +0300

    EPMRDRUTVE-1146 Added return to switch cases

--- src/main/java/com/epam/architecture/datasource/JSONReader.java
+++ src/main/java/com/epam/architecture/datasource/JSONReader.java
@@ -27,26 +27,20 @@ public class JSONReader<T extends Serializable> {
         if (!fileToRead.exists()) {
             throw new RestoreFromDataSourceException();
         }
-        T[] deserializableObjects;
         switch (type) {
             case ACCOUNT:
-                deserializableObjects = (T[]) mapper.readValue(fileToRead, User[].class);
-                break;
+                return (T[]) mapper.readValue(fileToRead, User[].class);
             case AUTHOR:
-                deserializableObjects = (T[]) mapper.readValue(fileToRead, Author[].class);
-                break;
+                return  (T[]) mapper.readValue(fileToRead, Author[].class);
             case BOOK:
-                deserializableObjects = (T[]) mapper.readValue(fileToRead, Book[].class);
-                break;
+                return  (T[]) mapper.readValue(fileToRead, Book[].class);
             case BOOKMARK:
-                deserializableObjects = (T[]) mapper.readValue(fileToRead, Bookmark[].class);
-                break;
+                return  (T[]) mapper.readValue(fileToRead, Bookmark[].class);
             default:
                 RestoreFromDataSourceException exception = new RestoreFromDataSourceException();
                 logger.error("Failed to take data", exception);
                 throw exception;
         }
-        return deserializableObjects;
     }
 
     public String getPathForRead() {
