ccacead3e32d324913c59c27dc21ace67fe52dec
Nikolai Polevoi <nikolai_polevoi@epam.com>
2022-01-30 22:39:59 +0300

    Implementation of the class for checking the administrator's rights

--- src/main/java/com/epam/architecture/webapp/servlets/AdminFilter.java
+++ src/main/java/com/epam/architecture/webapp/servlets/AdminFilter.java
@@ -2,7 +2,6 @@ package com.epam.architecture.webapp.servlets;
 
 import com.epam.architecture.userinterface.LibraryService;
 import com.epam.architecture.webapp.commands.CookieCommand;
-import com.epam.architecture.webapp.commands.WebCommandFactory;
 import com.epam.architecture.webapp.util.LibraryWebWorker;
 
 import javax.servlet.FilterChain;
@@ -13,16 +12,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/jsp/*"})
-public class UserFilter extends HttpFilter {
-
+@WebFilter(urlPatterns = {"/jsp/adminRights/*"})
+public class AdminFilter extends HttpFilter {
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         String login = (String) request.getSession().getAttribute(CookieCommand.LOGIN_NAME);
-        String password = (String) request.getSession().getAttribute(CookieCommand.PASSWORD_NAME);
         LibraryService libraryService = LibraryWebWorker.takeLibraryService();
-        if (login == null || password == null || !libraryService.logInAccount(login, password)) {
-            request.getRequestDispatcher("login.jsp").forward(request, response);
+        if (login == null || !libraryService.userIsAdmin(login)) {
+            request.getRequestDispatcher("../main.jsp").forward(request, response);
 
         }
         filterChain.doFilter(request, response);
